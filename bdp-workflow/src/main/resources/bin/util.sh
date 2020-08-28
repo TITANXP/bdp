009de291aad92f78bb49cc23bf4733f6cd8b17b5
TITANXP <1619450746@qq.com>
2020-08-28 11:03:54 +0800

    add bdp-stream bdp-workflow.sh

---
+++ bdp-workflow/src/main/resources/bin/util.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+printHeading() {
+    title="$1"
+    paddingWidth=$((($(tput cols)-${#title})/2-3))
+    printf "\n%${paddingWidth}s"|tr ' ' '='
+    printf "  $title  "
+    printf "%${paddingWidth}s\n\n"|tr ' ' '='
+}
+
+showTime() {
+    date '%FT%H:%M%z'
+}
